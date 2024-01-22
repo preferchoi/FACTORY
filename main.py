3b0d81d7fd338bbf7bd47f27f89749c375635b32
prefer_choi <prefer_choi@naver.com>
2024-01-22 17:04:39 +0900

    feat: machine 관련 기본 api 추가

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 from fastapi import FastAPI
-import Managers
 from Factories import Factories
 from Machines import Machines
 
 app = FastAPI()
 
 Factory = Factories()
-Factory_manager = Managers.InstanceManager()
-Factory_manager.add_instance(Factory)
 
-for _ in range(3):
-    Factory.add_machine(Machines())
 
 @app.get("/get_factories")
 async def get_factories():
-    """
-    현재 생성된 공장의 id 값을 반환하는 api입니다.
-    :return: 현재 생성된 공장의 id
-    """
-    return Factory_manager.get_instances()
+    return Factory.instance
 
 
 @app.get("/get_factory/{factory_id}")
 async def get_factory(factory_id: int):
-    """
-    특정 id 값을 가진 공장 인스턴스를 반환하는 api입니다.
-    :param factory_id: 특정 공장 인스턴스의 id
-    :return: 해당 id를 가진 인스턴스가 없을 경우 error 반환, 있을 경우 공장 인스턴스 반환
-    """
-    return Factory_manager.get_instance(factory_id)
+    try:
+        return Factory.instance[factory_id]
+    except KeyError:
+        return {"status": "fail"}
+
+
+@app.get("/get_machines")
+async def get_machines():
+    return Machines.instance
+
+
+@app.get("/get_machine/{machine_id}")
+async def get_machine(machine_id: int):
+    try:
+        return Machines.instance[machine_id]
+    except KeyError:
+        return {"status": "fail"}
