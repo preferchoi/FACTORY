48aabf86af61e3e13838470e4c4312d46e8bbaa2
prefer_choi <prefer_choi@naver.com>
2024-01-22 12:35:31 +0900

    feat: 생성 시 기계 추가, 공장 목록 및 상세 데이터 반환 구현

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import Managers
 from Factories import Factories
+from Machines import Machines
 
 app = FastAPI()
 
@@ -8,22 +9,23 @@ Factory = Factories()
 Factory_manager = Managers.InstanceManager()
 Factory_manager.add_instance(Factory)
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
+for _ in range(3):
+    Factory.add_machine(Machines())
 
 @app.get("/get_factories")
 async def get_factories():
-    return {"Factory": [{"id": instance.id} for instance in Factory_manager.get_instances()]}
+    """
+    현재 생성된 공장의 id 값을 반환하는 api입니다.
+    :return: 현재 생성된 공장의 id
+    """
+    return Factory_manager.get_instances()
 
 
-@app.get("/get_factory")
-async def get_factory():
-    return {"Factory": Factory_manager.get_instances()}
+@app.get("/get_factory/{factory_id}")
+async def get_factory(factory_id: int):
+    """
+    특정 id 값을 가진 공장 인스턴스를 반환하는 api입니다.
+    :param factory_id: 특정 공장 인스턴스의 id
+    :return: 해당 id를 가진 인스턴스가 없을 경우 error 반환, 있을 경우 공장 인스턴스 반환
+    """
+    return Factory_manager.get_instance(factory_id)
