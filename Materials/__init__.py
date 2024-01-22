219e7f98d1420e2354e1edca36a118e8a371db71
prefer_choi <prefer_choi@naver.com>
2024-01-22 17:06:07 +0900

    feat: 재료 테마 채택 => 철강산업

--- Materials/__init__.py
+++ Materials/__init__.py
@@ -1,2 +1,17 @@
 class Materials:
-    materials = {'water': 100}
+    materials = {
+        'money': {"cost": 1, "counter": 10000},
+        "iron_ore": {"cost": 1, "counter": 0},
+        "coal": {"cost": 1, "counter": 0},
+        "sintered_steel": {"cost": 1, "counter": 0},
+        "coke": {"cost": 1, "counter": 0},
+        "slag": {"cost": 1, "counter": 0},
+        "steel": {"cost": 1, "counter": 0},
+        "steel_Slab": {"cost": 1, "counter": 0},
+        "hot_rolled_plate": {"cost": 1, "counter": 0},
+        "cold_rolled_plate": {"cost": 1, "counter": 0},
+        "steel_billet": {"cost": 1, "counter": 0},
+        "wire_rod": {"cost": 1, "counter": 0},
+        "casting": {"cost": 1, "counter": 0},
+        "forging": {"cost": 1, "counter": 0},
+    }
