4afc9ea96cb20ae86d80b8e2c583f4128ae486a5
prefer_choi <prefer_choi@naver.com>
2024-01-23 16:01:25 +0900

    feat: 상품 구매/판매 api 작성

--- main.py
+++ main.py
@@ -64,6 +64,33 @@ async def get_materials():
     return Materials.materials
 
 
+@app.get("/materials/buy")
+async def material_buy(material: str, counter: int):
+    try:
+        total_cost = counter * Materials.materials[material]["cost"]
+        if Materials.materials["money"]["counter"] >= total_cost:
+            Materials.materials["money"]["counter"] -= total_cost
+            Materials.materials[material]["counter"] += counter
+            data_update_event.set()
+            return {"status": "success"}
+        return {"status": "fail"}
+    except KeyError:
+        return {"status": "KeyError"}
+
+
+@app.get("/materials/sell")
+async def material_sell(material: str, counter: int):
+    try:
+        if Materials.materials[material]["counter"] >= counter:
+            Materials.materials[material]["counter"] -= counter
+            Materials.materials["money"]["counter"] += counter * Materials.materials[material]["cost"]
+            data_update_event.set()
+            return {"status": "success"}
+        return {"status": "fail"}
+    except KeyError:
+        return {"status": "KeyError"}
+
+
 @app.get("/materials_sse")
 async def materials_sse():
     return StreamingResponse(send_materials(), media_type="text/event-stream")
