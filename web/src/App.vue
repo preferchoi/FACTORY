6c28bdcda837f8ba71dc068eca57b585d8ece243
prefer_choi <prefer_choi@naver.com>
2024-01-24 09:36:27 +0900

    init: client 서버 시작

---
+++ web/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  methods: {
+  },
+}
+</script>
+
+<style></style>
