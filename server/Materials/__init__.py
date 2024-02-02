488c84b4ecb05818b43418b1bc5ab6b13f50fa6e
prefer_choi <prefer_choi@naver.com>
2024-02-02 16:55:46 +0900

    fix: 이름 소문자 통일 Slab => slab

--- server/Materials/__init__.py
+++ server/Materials/__init__.py
@@ -7,7 +7,7 @@ class Materials:
         "cokes": {"cost": 700, "counter": 0},
         "slag": {"cost": 500, "counter": 0},
         "steel": {"cost": 3000, "counter": 0},
-        "steel_Slab": {"cost": 4000, "counter": 0},
+        "steel_slab": {"cost": 4000, "counter": 0},
         "hot_rolled_plate": {"cost": 5000, "counter": 0},
         "cold_rolled_plate": {"cost": 6000, "counter": 0},
         "steel_billet": {"cost": 7000, "counter": 0},
