acdac1a1c7a69b4729e0b1156b74a4e53670e4a7
prefer_choi <prefer_choi@naver.com>
2024-02-14 08:26:15 +0900

    feat: 기계 open/close 가능하게 만듦

--- web/src/components/MachineComponent.vue
+++ web/src/components/MachineComponent.vue
@@ -1,27 +1,46 @@
 <template>
     <v-card>
-        <v-card-title>{{ machine.id }}번 기계</v-card-title>
+        <v-card-title class="d-flex justify-space-between">
+            <span>{{ machine.id }}번 기계</span>
+            <v-icon v-if="isOpen" @click="isOpen=!isOpen">mdi-close</v-icon>
+            <v-icon v-if="!isOpen" @click="isOpen=!isOpen">mdi-plus</v-icon>
+        </v-card-title>
         <v-card-subtitle>소모 시간 - {{ machine.process_time }}s / 불량률 - {{ machine.error_rate * 100 }}%</v-card-subtitle>
         <v-card-text>
             <v-divider :thickness="1" style="margin-bottom: 10px;"></v-divider>
             <text class="d-flex justify-center">투입</text>
-            <v-row>
+            <v-row v-if="isOpen">
                 <v-col cols="4" v-for="(quantity, item) in machine.input_items" :key="item">
-                    <img :src="getImagePath(item)" :alt="item" style="width: 100%;height: auto;" /> 
+                    <img :src="getImagePath(item)" :alt="item" style="width: 100%;height: auto;" />
                     <text class="d-flex justify-center">{{ item }}: {{ quantity }}</text>
                 </v-col>
             </v-row>
+
+            <v-row v-if="!isOpen">
+                <v-col cols="4" v-for="(quantity, item) in machine.input_items" :key="item">
+                    <text class="d-flex justify-center">{{ item }}: {{ quantity }}</text>
+                </v-col>
+            </v-row>
+
             <v-divider :thickness="1" style="margin-top: 10px; margin-bottom: 10px;"></v-divider>
+
             <text class="d-flex justify-center">반환</text>
-            <v-row>
+            
+            <v-row v-if="isOpen">
                 <v-col cols="4" v-for="(quantity, item) in machine.output_items" :key="item">
-                    <img :src="getImagePath(item)" :alt="item" style="width: 100%;height: auto;" /> 
+                    <img :src="getImagePath(item)" :alt="item" style="width: 100%;height: auto;" />
                     <text class="d-flex justify-center">{{ item }}: {{ quantity }}</text>
                 </v-col>
             </v-row>
-            
+
+            <v-row v-if="!isOpen">
+                <v-col cols="4" v-for="(quantity, item) in machine.output_items" :key="item">
+                    <text class="d-flex justify-center">{{ item }}: {{ quantity }}</text>
+                </v-col>
+            </v-row>
+
             <v-divider :thickness="1" style="margin-top: 10px;"></v-divider>
-            
+
             <v-card-actions class="d-flex justify-end">
                 <v-btn v-if="loading" class="bg-grey-darken-3">
                     loading
@@ -31,7 +50,7 @@
                 </v-btn>
             </v-card-actions>
         </v-card-text>
-        
+
     </v-card>
 </template>
 
@@ -40,6 +59,7 @@ export default {
     name: "MachineComponent",
     data() {
         return {
+            isOpen:true,
             loading: false,
         }
     },
