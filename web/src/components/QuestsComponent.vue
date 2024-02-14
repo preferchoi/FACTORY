7b738efcb85e86df1805773537e65fc95049987f
prefer_choi <prefer_choi@naver.com>
2024-02-14 13:45:49 +0900

    feat: 모든 퀘스트 완료 시 문구 추가

--- web/src/components/QuestsComponent.vue
+++ web/src/components/QuestsComponent.vue
@@ -1,24 +1,34 @@
 <template>
     <div>
         <v-card class="quest" v-for="(value, key) in quests" :key=key :rounded="'xl'">
-            <v-card-title>
-                No.{{ key }}
-            </v-card-title>
-            <v-card-text>
-                <text>다음 물품을 구해오시오.</text>
-                <v-chip v-for="(quest_value, quest_key) in value.quest" :key="quest_key" style="margin-bottom: 5px;">
-                    <img :src="getImagePath(quest_key)" :alt="item" style="width:15%;height:auto%;" />
-                    :{{ quest_value }}
-                </v-chip>
-                <text>보상</text>
-                <v-chip v-for="(reword_value, reword_key) in value.reword.output_items" :key="reword_key"
-                    style="margin-bottom: 10px;">
-                    <img :src="getImagePath(reword_key)" :alt="item" style="width:15%;height:auto%;" /> - 제작 기계
-                </v-chip>
-                <v-card-actions class="d-flex justify-end">
-                    <v-btn class="bg-grey-darken-3" @click="quest_clear(key)">완료</v-btn>
-                </v-card-actions>
-            </v-card-text>
+            <div v-if="key!=0">
+                <v-card-title>
+                    No.{{ key }}
+                </v-card-title>
+                <v-card-text>
+                    <text>다음 물품을 구해오시오.</text>
+                    <v-chip v-for="(quest_value, quest_key) in value.quest" :key="quest_key" style="margin-bottom: 5px;">
+                        <img :src="getImagePath(quest_key)" :alt="item" style="width:15%;height:auto%;" />
+                        :{{ quest_value }}
+                    </v-chip>
+                    <text>보상</text>
+                    <v-chip v-for="(reword_value, reword_key) in value.reword.output_items" :key="reword_key"
+                        style="margin-bottom: 10px;">
+                        <img :src="getImagePath(reword_key)" :alt="item" style="width:15%;height:auto%;" /> - 제작 기계
+                    </v-chip>
+                    <v-card-actions class="d-flex justify-end">
+                        <v-btn class="bg-grey-darken-3" @click="quest_clear(key)">완료</v-btn>
+                    </v-card-actions>
+                </v-card-text>
+            </div>
+            <div v-if="key==0">
+                <v-card-title>
+                    모든 퀘스트 완료
+                </v-card-title>
+                <v-card-text>
+                    <text>가장 효율적인 공장을 구현해보세요.</text>
+                </v-card-text>
+            </div>
         </v-card>
     </div>
 </template>
